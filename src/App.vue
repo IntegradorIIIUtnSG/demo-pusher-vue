52918adeb49c3138c00df10029ce6bf1de1ac5a5
Alonso Chavarria <alons182@hotmail.com>
2026-07-12 20:04:30 -0600

    Add Pusher integration and update dependencies
    - Configure Pusher in App.vue using Laravel Echo
    - Add .env.example for environment variables
    - Update .gitignore to include .env
    - Add new dependencies: laravel-echo and pusher-js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+
+import { configureEcho } from "@laravel/echo-vue";
+import { useEcho } from "@laravel/echo-vue";
+ 
+
+configureEcho({
+    broadcaster: "pusher",
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+    authEndpoint: import.meta.env.VITE_PUSHER_AUTH_ENDPOINT,
+    bearerToken: import.meta.env.VITE_TEMP_TOKEN // idealmente deberia ser un token de usuario logueado, pero para el ejemplo lo pongo fijo
+});
+
+
+let orderId = 1;
+
+useEcho(
+    `orders.${orderId}`,
+    "OrderShipmentStatusUpdated",
+    (e) => {
+        console.log(e.order);
+    },
+);
+
 </script>
 
 <template>
